00ee1863101f4fbbb15d82c8fae921ec356bc3c5
keshan99 <mwmadushakeshan99@gmail.com>
2021-07-13 13:51:51 +0530

    init

---
+++ app.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const path = require("path");
+const mysql = require("mysql");
+const dotenv = require('dotenv');
+const cookieParser = require('cookie-parser');
+const fileUpload = require('express-fileupload');
+
+dotenv.config({ path: __dirname + '/.env' });
+
+const app = express();
+
+const db = mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    port: process.env.DATABASE_PORT,
+    database: process.env.DATABASE
+});
+
+
+const publicDirectory = path.join(__dirname, './public')
+app.use(express.static(publicDirectory));
+
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cookieParser());
+
+//image part
+app.use(fileUpload());
+
+
+
+app.set('view engine', 'hbs');
+
+
+db.connect((error) => {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log("My sql connected...");
+    }
+})
+
+app.listen(5000, () => {
+    console.log('Server started on port 5000');
+});
